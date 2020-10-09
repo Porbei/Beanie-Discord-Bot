a14a0c316b21e10b629f1134d1bdf355bb11c99f
Lewis <70801823+Porbei@users.noreply.github.com>
2020-10-09 22:08:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //This is the main file for the bot, written in discord.js. Don't worry, I haven't exposed the token since it's in a different file. If you want to hide your token when publishing just find a tutorial lol idk.
+
 const { Discord, Client, MessageEmbed } = require('discord.js')
 require('dotenv').config()
 const prefix = process.env.PREFIX;
@@ -25,7 +26,7 @@ client.on('message', async (message) => {
   .setColor('#0099ff')
   .setAuthor(message.author.tag, message.author.avatarURL())
 	.setTitle('Beanie - Help')
-  .setDescription('**General Commands:** \n Get help **-** *help \n Github Link **-** *git \n **Random Commands:** \n Send an image of a bean **-** *bean')
+  .setDescription('**General Commands:** \n Get Help **-** *help \n Github Link **-** *git \n **Random Commands:** \n Send An Image Of A Pool Of Beans **-** *bean \n Omegle Kid Meme **-** *omeglekid')
 	.setFooter('Bot Made By Porbei#4444');
   message.channel.send(embed)
 }
@@ -37,5 +38,14 @@ client.on('message', async (message) => {
   message.channel.send("You want the source code? Well here you go then: https://github.com/Porbei/Beanie-Discord-Bot")
 }
 })
+
+//Omegle Kid
+client.on('message', async (message) => {
+  if(message.content === `${prefix}omeglekid`) {
+  message.channel.send("Um, sure. Here you go: https://cdn.discordapp.com/attachments/753721434441777172/764158537650667550/video0.mp4")
+}
+})
+
+
 
 client.login(process.env.TOKEN);
